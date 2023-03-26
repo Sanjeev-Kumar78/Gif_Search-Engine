c57e7a184fc69f8bdaf774a09b26a172b160ebdc
Sanjeev-Kumar78 <premchandrasingh786@gmail.com>
2023-03-26 23:25:34 +0530

    Chnages Made

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Gif_Search-Engine
+# Gif & Sticker Search
+[![Netlify Status](https://api.netlify.com/api/v1/badges/8811cc2f-2fb9-4ae5-a969-73625461d818/deploy-status)](https://app.netlify.com/sites/boisterous-sunshine-473af2/deploys)
+
+#### • A web app to search for gifs and stickers.
+
+#### • Built with CSS, HTML, JS and Giphy API.
+#### • [Live Demo]()
+
+## How to Use ?
+1. Clone the repo
+2. Go to the directory & open js folder
+3. Edit `script.js` file 1st line with your API Key ```var api = "key"```
+4. Replace `key` with your API Key
+5. Open index.html in your browser
+
+## How to get API Key ?
+1. Go to [Giphy Developers](https://developers.giphy.com/)
+2. Create an account
+3. Go to [Dashboard](https://developers.giphy.com/dashboard/)
+4. Create an app with any name
+5. Copy the API Key
+
+## Screenshots
+
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import { api } from './data.js';
+var api = "2wRlIuIZQn7N6V8EhtxGOW36eJDGyTu6"
 // Search Button and Enter Key
 document.querySelector(".container-button").addEventListener('click', function () {
     var input = document.querySelector("input").value;
@@ -15,8 +15,11 @@ document.querySelector("input").addEventListener('keydown', function (event) {
 
 function AJAX(input) {
     var userask = prompt("Ex: Stickers or Gifs:").toLowerCase();
-    if (userask == "stickers" || userask == "gifs") {
-    var url = "https://api.giphy.com/v1/"+userask+"/search?q=" + input + "&api_key="+api+"&limit=10";
+    if (userask == "gifs" || userask == "gif") {
+    var url = "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key="+api+"&limit=10";
+    }
+    else if (userask == "stickers" || userask == "sticker") {
+        var url = "https://api.giphy.com/v1/stickers/search?q=" + input + "&api_key="+api+"&limit=10";
     }
     else {
         alert("Please enter Stickers or Gifs:");
@@ -27,15 +30,15 @@ function AJAX(input) {
     GiphyAJAXCall.send();
     GiphyAJAXCall.addEventListener('load', function (e) {
         var data = e.target.response;
-        pushToDOM(data, input);
+        pushToDOM(data, input,userask);
     })
 };
 
 
 // Push to DOM 
-function pushToDOM(data,input) {
+function pushToDOM(data,input,userask) {
     var container = document.querySelector(".container-results");
-    container.innerHTML = "<h2>" + input + "</h2>";
+    container.innerHTML = "<h2>" + input +" "+userask+ "</h2>";
     var response = JSON.parse(data);
     response.data.forEach(function (image) {
         var src = image.images.fixed_height.url;
